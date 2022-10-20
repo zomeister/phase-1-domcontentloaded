e690af2311d043335808470f111028d089403762
Zoe Guidry <oyezoey@gmail.com>
2022-10-20 02:04:19 -0500

    commit tree acfcec43bb5f87f01e48497126475644745221f2

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+const text = document.querySelector('#text');
+
+document.addEventListener("DOMContentLoaded", function() {
+    text.textContent = "This is really cool!";
+    });
+
+text.textContent = "JavaScript is so cool. It lets me add text to my page programmatically.";
